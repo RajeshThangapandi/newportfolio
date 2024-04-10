704d9a5388c3710875c18558b6dbe4ecd2a88d00
Raj <tr98523@gmail.com>
2024-04-10 20:37:50 +0530

    setupcors

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
 // vite.config.js
-export default {
-    server: {
-      proxy: {
-        // Proxy options
-      },
-      cors: true, // Enable CORS for development server
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    minify: true,
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
     },
-  };
-  
+    cors: {
+       origin: 'https://rajeshthangapandi.github.io', // Allow requests from any origin
+        methods: 'GET, POST, PUT, DELETE', // Allow specified HTTP methods
+        allowedHeaders: '*', // Allow any headers
+        exposedHeaders: '', // Expose no additional headers
+        credentials: false, // Allow credentials (cookies, etc.)
+        maxAge: 3600, // Cache preflight requests for 1 hour (optional)
+        preflightContinue: false, // Disable preflight requests (optional)
+        optionsSuccessStatus: 204 // Set the status code for successful OPTIONS requests (optional)
+      }
+  },
+  plugins: [
+    // Add plugins here
+  ]
+});
